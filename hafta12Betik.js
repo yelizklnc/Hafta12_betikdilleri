bce7545b20a0e2a434e3f225ecb92e87e2265b12
Yeliz Kılınç <yelizklnc6262@gmail.com>
2026-04-25 14:07:33 +0300

    Create hafta12Betik.js

---
+++ hafta12Betik.js
@@ -0,0 +1,29 @@
+var a=5;
+undefined
+
+var b=6;
+undefined
+
+console.log(a*b)
+30
+******************************************************
+var slogan="Yaşasın Türkiye";
+undefined
+alert (slogan);
+undefined
+alert(slogan.length);
+undefined
+function b(metin) { 
+       for(i=0;i<metin.length;i++){   
+          alert(metin[i]);                                                                                                                                                                                                                                                }                                                                                                                                                                                                                                                                   alert(metin);                                                                                                                                                                                                                                               }
+undefined
+b(slogan)
+undefined //bu kodda her basışta harfler tek tek gelir en sonda birleşir
+*******************************************************
+var m="Yaşasın Türkiye!)
+function foo(parametre){
+    slg=''
+    slg=slg+parametre[i]
+    alert(slg)
+}} foo(m)   // bu kod her seferinde 1 harfi yanına ekler ör: birincisinde Y ikincisinde Ya üçüncüsünde Yaş gibi 
+                                                                                                                                                                                                                                                    
